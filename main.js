8142e613f6f4db4f7f49e9adbd340efc1f184d76
felipe.Labarca() <felipe.labarca.c@gmail.com>
2025-12-21 19:28:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+ window.addEventListener('load', () => {
+            document.body.style.opacity = '0';
+            setTimeout(() => {
+                document.body.style.transition = 'opacity 0.8s ease';
+                document.body.style.opacity = '1';
+            }, 100);
+        });
